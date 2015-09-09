8df7cc6929f58c1c06ae047e4410fd4c650cfb33
Erik Luetkehans <eluetekhans@gmail.com>
2015-09-09 10:02:54 -0700

    add test template

--- tcp_server/test/tcp_test.js
+++ tcp_server/test/tcp_test.js
@@ -1,12 +1,19 @@
 var expect = require('chai').expect;
-var tcp = require(__dirname + "/../server");
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+require(__dirname + '/../server');
 
 describe('TCP server', function() {
-  before(function() {
-    tcp.listen(8000);
-  });
-
-  after(function() {
-    tcp.close();
+  it('should respond to a request', function(done) {
+    chai.request('localhost:8888')
+      .get('/anywhere')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(res.text).to.eql('');
+        done();
+      });
   });
 });
+
+//send test data, see if files in directory increased by one.
