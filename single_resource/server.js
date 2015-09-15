bcbe39277b311d6309bf61b9e7f239cfd498211e
Erik Luetkehans <eluetekhans@gmail.com>
2015-09-14 17:03:55 -0700

    intitial commit

---
+++ single_resource/server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var router = express.Router();
+var port = 3000;
+var server = http.createServer();
---
+++ single_resource/test/server_test.js
@@ -0,0 +1,5 @@
+var chai = require('chai');
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+
+require(__dirname + '/../server.js');
